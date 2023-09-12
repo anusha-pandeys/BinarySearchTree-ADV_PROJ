27eb59c9559cc2f59c2012ddd9e59119a0f584b1
s-AOZDEMIR <s-AOZDEMIR@487M124637.lwsd.k12wa.org>
2023-09-12 10:44:51 -0700

    commit 1

--- BST Simulator/src/BSTSimulator.java
+++ BST Simulator/src/BSTSimulator.java
@@ -4,6 +4,8 @@
  * You should add additional classes as appropriate to support good modularity and reduce redundancy.
  *
  */
+
+// hiiiii
 public class BSTSimulator {
 	public static void main() {
 		
